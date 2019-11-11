345575503995601137b47ee853f67f0948831ede
pashasaley <pashasaley@gmail.com>
2019-11-11 18:00:27 +0300

    Errors for blockUser.

--- home_page/views.py
+++ home_page/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from editor.models import TShirts, get_tags
+from admin_page.models import BlockUser
+from django.http import HttpResponseForbidden
 
 
 def add_tags(request):
@@ -9,6 +11,10 @@ def add_tags(request):
         tags = get_tags()
         return render(request, 'home_page.html', {'queryset': queryset, 'tags': tags})
     else:
+        user = request.user.id
+        if user != '':
+            if BlockUser.objects.get(id_user=user).block:
+                return HttpResponseForbidden()
         queryset = TShirts.objects.all().order_by('-id')[:9]
         tags = get_tags()
         return render(request, 'home_page.html', {'queryset': queryset, 'tags': tags})
